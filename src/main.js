bb17e010048218596426a3be05c6ff07242e2850
康康 <hankang961@163.com>
2021-06-05 02:56:26 +0800

    Swiper区域开发

--- src/main.js
+++ src/main.js
@@ -2,14 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+
 import fastClick from 'fastclick'
+fastClick.attach(document.body)
+
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
 
-fastClick.attach(document.body)
 
 new Vue({
   router,
---
+++ src/views/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="slide in swiperSlides" :key="slide">
+        <img :src="slide" class="swiper-img" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        loop: true,
+      },
+      swiperSlides: [
+        "https://img1.qunarzz.com/qs/2103/22/dfd49774c7a1f102.jpg",
+        "https://img1.qunarzz.com/qs/2101/87/f3fa48c8346dab02.jpg",
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.wrapper ::v-deep .swiper-pagination-bullet-active{
+    background: red !important; 
+}
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 54.4%;
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
