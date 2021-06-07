f36aac88c2e59e2a83628fd030255ec9e456bcdd
康康 <hankang961@163.com>
2021-06-07 10:44:52 +0800

    列表布局

---
+++ src/views/city/components/List.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="list">
+    <div class="area">
+      <div class="title border-topbottom">当前城市</div>
+      <div class="btn-list">
+        <div class="btn-wrapper">
+          <div class="btn">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title bordertopbottom">当前城市</div>
+      <div class="btn-list">
+        <div class="btn-wrapper">
+          <div class="btn">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.border-topbottom {
+  &:before {
+    border-color: #ccc;
+  }
+  &:after {
+    border-color: #ccc;
+  }
+}
+.border-bottom {
+  &:before {
+    border-color: #ccc;
+  }
+}
+.list {
+    overflow: hidden;
+    position: absolute;
+    top: 1.58rem;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  .title {
+    line-height: 0.44rem;
+    background: #eee;
+    padding-left: 0.2rem;
+    color: #666;
+    font-size: 0.26rem;
+  }
+  .btn-list {
+    overflow: hidden;
+    padding: 0.1rem 1rem 0.1rem 0.1rem;
+    .btn-wrapper {
+      float: left;
+      width: 33.33%;
+      .btn {
+        margin: 0.1rem;
+        padding: 0.1rem 0;
+        text-align: center;
+        border: 0.02rem solid #ccc;
+        border-radius: 0.06rem;
+      }
+    }
+  }
+  .item-list {
+    .item {
+      line-height: 0.76rem;
+      color: #666;
+      padding-left: 0.2rem;
+    }
+  }
+}
+</style>
