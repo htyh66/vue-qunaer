daff391c4c80aa4f685a35ca01c0e8f524980ce7
康康 <hankang961@163.com>
2021-06-10 23:11:43 +0800

    header渐隐渐现

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,18 +1,24 @@
 <template>
-  <div>
+  <div class="content">
       <detail-banner></detail-banner>
+      <detail-header></detail-header>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner.vue';
+import DetailHeader from './components/Header.vue';
 export default {
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.content{
+  height: 50rem;
+}
+    
 </style>
