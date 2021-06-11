8fc9f0b8b123a1ea7332c10fa6fd82f997745cc3
康康 <hankang961@163.com>
2021-06-11 17:42:07 +0800

    递归组件

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,17 +2,57 @@
   <div class="content">
       <detail-banner></detail-banner>
       <detail-header></detail-header>
+      <detail-list :list="list"></detail-list>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner.vue';
 import DetailHeader from './components/Header.vue';
+import DetailList from './components/List.vue';
 export default {
   components: {
     DetailBanner,
-    DetailHeader
-  }
+    DetailHeader,
+    DetailList
+  },
+  data() {
+    return {
+      list : [
+        {
+          title : "1111",
+          children : [
+            {
+              title : '2222222222',
+              children : [
+                {
+                  title: '3333333'
+                }
+              ]
+            },{
+              title : '2222222222',
+              children : [
+                {
+                  title: '3333333'
+                }
+              ]
+            },{
+              title : '2222222222',
+              children : [
+                {
+                  title: '3333333'
+                }
+              ]
+            }
+          ]
+        },{
+          title : "1111",
+        },{
+          title : "1111",
+        },
+      ],
+    }
+  },
 }
 </script>
 
